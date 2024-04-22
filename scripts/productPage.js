d0b9c7aa60b92987973ea3056c796c59d0ee268a
NataliaD92 <natalia.d.051992@gmail.com>
2024-04-22 02:44:58 +0200

    Main page update

---
+++ scripts/productPage.js
@@ -0,0 +1,8 @@
+//Przycisk powrotu
+
+// const $mainPageBtn = document.getElementById('go-to-mainPage')
+
+// $mainPageBtn.addEventListener('click', () => {
+// 	window.location.href = 'index.html'
+// })
+
